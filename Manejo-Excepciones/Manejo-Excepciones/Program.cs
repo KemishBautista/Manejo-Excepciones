e6716f67e304ddbccecd0a1af9d358117c7b8a63
KemishBautista <kembaga44@gmail.com>
2022-05-24 11:25:33 -0700

    Manejo-Excepciones
    Programa 2

---
+++ Manejo-Excepciones/Manejo-Excepciones/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manejo_Excepciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("--------POTENCIAS--------");
+                Console.Write("Ingrese un numero entero: ");
+                int Numero = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("El numero al cuadrado es: " + Math.Pow(Numero, 2));
+                Console.WriteLine("Presione enter para continuar...");
+                Console.ReadKey();
+            }
+
+            catch(FormatException e)
+            {
+                Console.WriteLine("El valor ingresado no es un numero entero");
+                Console.ReadKey();
+            }
+            catch(OverflowException e)
+            {
+                Console.WriteLine("El numero ingresado es muy grande");
+                Console.ReadKey();
+            }
+        }
+    }
+}
